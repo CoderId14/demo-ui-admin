f0f34b33f0c4b913e143329dbbf8fc30c6c22c25
Hieu <danghieu14th@gmail.com>
2023-05-12 22:33:18 +0700

    update

--- src/environments.ts
+++ src/environments.ts
@@ -1,1 +1,2 @@
-export const API_HOST_NAME = 'https://demo-production-627b.up.railway.app/api/'
+// export const API_HOST_NAME = 'https://demo-production-627b.up.railway.app/api/'
+export const API_HOST_NAME = 'https://demo-jg34.onrender.com/api/'
