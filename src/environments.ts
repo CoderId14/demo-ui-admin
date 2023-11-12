879756fe90d28edccbd5f88f1235fcf85bf28630
Hieu <danghieu14th@gmail.com>
2023-11-12 11:21:30 +0700

    fix: api host

--- src/environments.ts
+++ src/environments.ts
@@ -1,3 +1,3 @@
 // export const API_HOST_NAME = 'https://demo-production-627b.up.railway.app/api/'
-// export const API_HOST_NAME = 'https://demo-jg34.onrender.com/api/'
-export const API_HOST_NAME = 'http://localhost:8080/api/'
+export const API_HOST_NAME = 'https://demo-jg34.onrender.com/api/'
+// export const API_HOST_NAME = 'http://localhost:8080/api/'
